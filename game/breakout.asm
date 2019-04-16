e6b9a2d671f75e6e278384522fae3cb42656a482
Patapoff <30930761+Patapoff@users.noreply.github.com>
2019-04-16 10:00:58 -0300

    Window Create Beginning / Icon

--- game/breakout.asm
+++ game/breakout.asm
@@ -5,23 +5,65 @@ option casemap :none
 include engine.inc
 
 .data
-    ;Estruturas dos jogadores:
-   ; player player <MAX_LIFE, 7, <IMG_SIZE, WIN_HT / 2, <0, 0>>>
+    ClassName db "WindowClass",0        ; nome da classe de janela
+    AppName db "Breakout",0        ;  Nome da Janela
 
-   canPlyrsMov pair <0, 0> ;Indica se cada jogador pode se mover
-
-   hit db FALSE ;Indica se algum jogador pontuo
-
-    over byte 0 ;Indica se o jogo acabou
+.DATA?                
+hInstance HINSTANCE ?        ; HANDLER
 
 .code 
 start:
     invoke GetModuleHandle, NULL
     mov hInstance, eax
 
-    invoke GameMain, hInstance, SW_SHOWDEFAULT
+    invoke WinCreate, hInstance, SW_SHOWDEFAULT
     invoke ExitProcess, eax
 
+WinCreate proc hInst:HINSTANCE, CmdShow:DWORD 
+    LOCAL wc:WNDCLASSEX                                            ; create local variables on stack 
+    LOCAL msg:MSG 
+    LOCAL hwnd:HWND
 
+    mov   wc.cbSize,SIZEOF WNDCLASSEX                   ; fill values in members of wc 
+    mov   wc.style, CS_HREDRAW or CS_VREDRAW 
+    mov   wc.lpfnWndProc, OFFSET WndProc 
+    mov   wc.cbClsExtra,NULL 
+    mov   wc.cbWndExtra,NULL 
+    push  hInstance 
+    pop   wc.hInstance 
+    mov   wc.hbrBackground,COLOR_WINDOW+1 
+    mov   wc.lpszMenuName,NULL 
+    mov   wc.lpszClassName,OFFSET ClassName 
+    invoke LoadIcon,hInstance, 
+    mov   wc.hIcon,eax 
+    mov   wc.hIconSm,eax 
+    invoke LoadCursor,NULL,IDC_ARROW 
+    mov   wc.hCursor,eax 
+    invoke RegisterClassEx, addr wc                       ; register our window class 
+    invoke CreateWindowEx,NULL,\ 
+                ADDR ClassName,\ 
+                ADDR AppName,\ 
+                WS_OVERLAPPEDWINDOW,\ 
+                CW_USEDEFAULT,\ 
+                CW_USEDEFAULT,\ 
+                CW_USEDEFAULT,\ 
+                CW_USEDEFAULT,\ 
+                NULL,\ 
+                NULL,\ 
+                hInst,\ 
+                NULL 
+    mov   hwnd,eax 
+    invoke ShowWindow, hwnd,CmdShow               ; display our window on desktop 
+    invoke UpdateWindow, hwnd                                 ; refresh the client area
+
+    .WHILE TRUE                                                         ; Enter message loop 
+                invoke GetMessage, ADDR msg,NULL,0,0 
+                .BREAK .IF (!eax) 
+                invoke TranslateMessage, ADDR msg 
+                invoke DispatchMessage, ADDR msg 
+   .ENDW 
+    mov     eax,msg.wParam                                            ; return exit code in eax 
+    ret 
+WinMain endp
 
 end start
