e5cd443e409c5a4dd45b6f037aa18bb0325567f8
Caio Rosa <caiopetruccirosa@gmail.com>
2019-04-17 15:44:57 -0300

    Minor changes

--- game/breakout.asm
+++ game/breakout.asm
@@ -9,96 +9,99 @@ include engine.inc
     header_msg              DB "Resultado", 0
     buffer                  DB 256 dup(?)
 
-
     player player_obj <<WIN_WD/2, WIN_HT-70>, 0, 4, 0>
-    ball ball_obj <<WIN_WD/2, WIN_HT-300>, <1, 1>>
+    ball ball_obj <<WIN_WD/2, WIN_HT-300>, <5, 5>>
     grid b_grid <>
 
 .code 
 start:
-    invoke GetModuleHandle, NULL
-    mov    hInstance, eax
+    INVOKE GetModuleHandle, NULL
+    MOV    hInstance, eax
 
-    invoke WinCreate, hInstance, SW_SHOWDEFAULT
-    invoke ExitProcess, eax
+    INVOKE WinCreate, hInstance, SW_SHOWDEFAULT
+    INVOKE ExitProcess, eax
 
 WinCreate proc hInst:HINSTANCE, CmdShow:DWORD 
-    local  wc:WNDCLASSEX                                            ; create local variables on stack 
-    local  msg:MSG 
-    local  hwnd:HWND
-    local  clientRect:RECT
+    LOCAL  wc:WNDCLASSEX                          ; create local variables on stack 
+    LOCAL  msg:MSG 
+    LOCAL  hwnd:HWND
+    LOCAL  clientRect:RECT
 
-    mov    wc.cbSize, SIZEOF WNDCLASSEX                   ; fill values in members of wc 
-    mov    wc.style, CS_HREDRAW or CS_VREDRAW 
-    mov    wc.lpfnWndProc, OFFSET WndProc 
-    mov    wc.cbClsExtra, NULL 
-    mov    wc.cbWndExtra, NULL 
+    MOV    wc.cbSize, SIZEOF WNDCLASSEX           ; fill values in members of wc 
+    MOV    wc.style, CS_HREDRAW or CS_VREDRAW 
+    MOV    wc.lpfnWndProc, OFFSET WndProc 
+    MOV    wc.cbClsExtra, NULL 
+    MOV    wc.cbWndExtra, NULL 
 
-    push   hInstance 
-    pop    wc.hInstance 
+    PUSH   hInstance 
+    POP    wc.hInstance 
 
-    mov    wc.hbrBackground, COLOR_WINDOW+3
-    mov    wc.lpszMenuName, NULL 
-    mov    wc.lpszClassName, OFFSET ClassName 
+    MOV    wc.hbrBackground, COLOR_WINDOW+3
+    MOV    wc.lpszMenuName, NULL 
+    MOV    wc.lpszClassName, OFFSET ClassName 
 
-    invoke LoadIcon, hInstance, 500
-    mov    wc.hIcon, eax 
-    mov    wc.hIconSm, 0
+    INVOKE LoadIcon, hInstance, 500
+    MOV    wc.hIcon, eax 
+    MOV    wc.hIconSm, 0
 
-    invoke LoadCursor, NULL, IDC_ARROW 
-    mov    wc.hCursor,eax 
-    invoke RegisterClassEx, addr wc                       ; register our window class 
+    INVOKE LoadCursor, NULL, IDC_ARROW 
+    MOV    wc.hCursor,eax 
+    INVOKE RegisterClassEx, addr wc               ; register our window class 
 
     ;======================================
 
-    mov    clientRect.left, 0
-    mov    clientRect.top, 0
-    mov    clientRect.right, WIN_WD
-    mov    clientRect.bottom, WIN_HT
+    MOV    clientRect.left, 0
+    MOV    clientRect.top, 0
+    MOV    clientRect.right, WIN_WD
+    MOV    clientRect.bottom, WIN_HT
 
-    invoke AdjustWindowRect, addr clientRect, WS_CAPTION, FALSE
+    INVOKE AdjustWindowRect, addr clientRect, WS_CAPTION, FALSE
 
-    mov    eax, clientRect.right
-    sub    eax, clientRect.left
-    mov    ebx, clientRect.bottom
-    sub    ebx, clientRect.top
+    MOV    eax, clientRect.right
+    SUB    eax, clientRect.left
+    MOV    ebx, clientRect.bottom
+    SUB    ebx, clientRect.top
 
     ;==============================
 
-    invoke CreateWindowEx, NULL, addr ClassName, addr AppName,\ 
+    INVOKE CreateWindowEx, NULL, addr ClassName, addr AppName,\ 
         WS_OVERLAPPED or WS_SYSMENU or WS_MINIMIZEBOX,\ 
         CW_USEDEFAULT, CW_USEDEFAULT,\
         eax, ebx, NULL, NULL, hInst, NULL 
         
-    mov    hwnd, eax 
-    invoke ShowWindow, hwnd, CmdShow               ; display our window on desktop 
-    invoke UpdateWindow, hwnd                      ; refresh the client area
+    MOV    hwnd, eax 
+    INVOKE ShowWindow, hwnd, CmdShow               ; Mostra a janela
+    INVOKE UpdateWindow, hwnd                      ; Atualiza a área da janela
 
-    .WHILE TRUE                                    ; Enter message loop 
-        invoke GetMessage, ADDR msg, NULL, 0, 0 
+    .WHILE TRUE                                    ; Loop de mensagem
+        INVOKE GetMessage, ADDR msg, NULL, 0, 0 
         .BREAK .IF (!eax) 
-        invoke TranslateMessage, ADDR msg 
-        invoke DispatchMessage, ADDR msg 
+        INVOKE TranslateMessage, ADDR msg 
+        INVOKE DispatchMessage, ADDR msg 
    .ENDW
 
-    mov  eax, msg.wParam                        ; return exit code in eax
-    ret
+    MOV    eax, msg.wParam                           ; Retorna o código de saída no eax
+    RET
 WinCreate endp
 
 WndProc proc _hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
-    .IF uMsg == WM_CREATE                                        ;Carrega as imagens e cria a thread principal:---------
-        invoke LoadAssets
-    .ELSEIF uMsg == WM_DESTROY                                   ; if the user closes our window 
-        invoke PostQuitMessage, NULL                             ; quit our application
-    .ELSEIF uMsg == WM_PAINT                                     ;Atualizar da página:-------------------------------  
-        invoke UpdateScreen, _hWnd
+    .IF uMsg == WM_CREATE                                          ; Carrega as imagens
+        INVOKE LoadAssets
+
+        ;MOV eax, OFFSET GameHandler 
+        ;INVOKE CreateThread, NULL, NULL, eax, 0, 0, ADDR threadID  ; Cria a thread principal
+        ;INVOKE CloseHandle, eax 
+    .ELSEIF uMsg == WM_DESTROY                                     ; Caso o jogador feche a janela
+        INVOKE PostQuitMessage, NULL                               ; Fecha o jogo
+    .ELSEIF uMsg == WM_PAINT      
+        INVOKE UpdateScreen, _hWnd                                 ; Atualizar a tela
     .ELSE
-        invoke DefWindowProc, _hWnd, uMsg, wParam, lParam         ; Default message processing 
-        ret
+        INVOKE DefWindowProc, _hWnd, uMsg, wParam, lParam          ; Messagem padrão
+        RET
     .ENDIF
 
-    xor eax, eax 
-    ret
+    XOR eax, eax 
+    RET
 WndProc endp
 
 LoadAssets proc ; Carrega os bitmaps e matriz de blocos do jogo:
@@ -107,71 +110,101 @@ LoadAssets proc ; Carrega os bitmaps e matriz de blocos do jogo:
     LOCAL layer_3:b_layer
     LOCAL layer_4:b_layer
 
-    invoke LoadBitmap, hInstance, BACKGROUND_BMP
-    mov    hBackgroundBmp, eax
+    INVOKE LoadBitmap, hInstance, BACKGROUND_BMP
+    MOV    hBackgroundBmp, eax
 
-    invoke LoadBitmap, hInstance, CELLS_BMP
-    mov    hCellsBmp, eax
+    INVOKE LoadBitmap, hInstance, CELLS_BMP
+    MOV    hCellsBmp, eax
 
-    invoke LoadBitmap, hInstance, BALL_BMP
-    mov    hBallBmp, eax
+    INVOKE LoadBitmap, hInstance, BALL_BMP
+    MOV    hBallBmp, eax
 
-    invoke LoadBitmap, hInstance, PLAYER_BMP
-    mov    hPlayerBmp, eax
+    INVOKE LoadBitmap, hInstance, PLAYER_BMP
+    MOV    hPlayerBmp, eax
 
-    mov layer_1.layer_index, 0
-    mov layer_2.layer_index, 1
-    mov layer_3.layer_index, 2
-    mov layer_4.layer_index, 3
-
-    ;xor ecx, ecx
-    ;.WHILE ecx < 18
-        ;mov layer_1.blocks[ecx], <>
-        ;mov layer_2.blocks[ecx], <>
-        ;mov layer_3.blocks[ecx], <>
-        ;mov layer_4.blocks[ecx], <>
-
-        ;inc ecx
-    ;.ENDW
-
-    ret
+    RET
 LoadAssets endp
 
+GameHandler proc p:dword
+GameHandler endp
+
 UpdateScreen proc _hWnd:HWND
-    local  ps:PAINTSTRUCT 
-    local  hDC:HDC 
-    local  hMemDC:HDC 
+    LOCAL  ps:PAINTSTRUCT 
+    LOCAL  hDC:HDC 
+    LOCAL  hMemDC:HDC 
 
-    invoke BeginPaint, _hWnd, addr ps
-    mov    hDC, eax
+    INVOKE BeginPaint, _hWnd, addr ps
+    MOV    hDC, eax
 
-    invoke CreateCompatibleDC, hDC
-    mov    hMemDC, eax
-
-
-    invoke SelectObject, hMemDC, hBackgroundBmp
-    invoke BitBlt, hDC, 0, 0, WIN_WD, WIN_HT, hMemDC, 0, 0, SRCCOPY
-
-    ;invoke SelectObject, hMemDC, hBackgroundBmp
-    ;xor ecx, ecx
-    ;.WHILE lengthof 
-
-    ;.ENDW
+    INVOKE CreateCompatibleDC, hDC
+    MOV    hMemDC, eax
 
 
-    invoke SelectObject, hMemDC, hPlayerBmp
-    movzx  eax, player.pos.x
-    movzx  ebx, player.pos.y
-    invoke BitBlt, hDC, eax, ebx, PLAYER_WD, PLAYTER_HT, hMemDC, 0, 0, SRCCOPY
+    INVOKE SelectObject, hMemDC, hBackgroundBmp
+    INVOKE BitBlt, hDC, 0, 0, WIN_WD, WIN_HT, hMemDC, 0, 0, SRCCOPY
 
-    invoke SelectObject, hMemDC, hBallBmp
-    movzx  eax, ball.pos.x
-    movzx  ebx, ball.pos.y
-    invoke BitBlt, hDC, eax, ebx, BALL_SIZE, BALL_SIZE, hMemDC, 0, 0, SRCCOPY
+    INVOKE SelectObject, hMemDC, hPlayerBmp
+    MOVZX  eax, player.pos.x
+    MOVZX  ebx, player.pos.y
+    SUB eax, PLAYER_WD/2
+    INVOKE BitBlt, hDC, eax, ebx, PLAYER_WD, PLAYTER_HT, hMemDC, 0, 0, SRCCOPY
 
-    invoke DeleteDC, hMemDC
-    invoke EndPaint, _hWnd, addr ps
-    ret
+    INVOKE SelectObject, hMemDC, hBallBmp
+    MOVZX  eax, ball.pos.x
+    MOVZX  ebx, ball.pos.y
+    SUB eax, BALL_SIZE/2
+    SUB ebx, BALL_SIZE/2
+    INVOKE BitBlt, hDC, eax, ebx, BALL_SIZE, BALL_SIZE, hMemDC, 0, 0, SRCCOPY
+
+
+    INVOKE DeleteDC, hMemDC
+    INVOKE EndPaint, _hWnd, addr ps
+    RET
 UpdateScreen endp
+
+UpdatePhysics proc
+    INVOKE MovePlayer, offset player
+    INVOKE MoveBall, offset ball
+UpdatePhysics endp
+
+MovePlayer proc _player:DWORD
+    ASSUME eax : ptr player_obj
+    MOV eax, _player
+
+    ; Incrementa o X
+    MOV bx, [eax].pos.x
+    MOV cx, [eax].speed
+
+    ADD bx, cx
+
+    MOV [eax].pos.x, bx
+
+    ASSUME eax:nothing
+    RET
+MovePlayer endp
+
+MoveBall proc _ball:DWORD
+    ASSUME eax : ptr ball_obj
+    MOV eax, _ball
+
+    ; Incrementa o X
+    MOV bx, [eax].pos.x
+    MOV cx, [eax].speed.x
+
+    ADD bx, cx
+
+    MOV [eax].pos.x, bx
+
+    ; Incrementa o Y
+    MOV bx, [eax].pos.y
+    MOV cx, [eax].speed.y
+
+    ADD bx, cx
+
+    MOV [eax].pos.y, bx
+
+    ASSUME eax:nothing
+    RET
+MoveBall endp
 
 end start
